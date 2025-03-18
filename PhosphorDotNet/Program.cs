ea13e7bdd24afa862972db8421d9c992ff09bb30
furriesRus <shousejackson@gmail.com>
2025-03-18 14:34:04 -0500

    Setup projects and main program

--- PhosphorDotNet/Program.cs
+++ PhosphorDotNet/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace PhosphorDotNet;
+﻿using System.Text.Json;
+
+namespace PhosphorDotNet;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length != 1)
+        {
+            throw new ArgumentException("Usage: PhosphorDotNet.exe <path>");
+        }
+
+        if (!Directory.Exists(args[0]))
+        {
+            throw new ArgumentException("Directory does not exist: " + args[0]);
+        }
+
+        string projectDetailsString;
+        
+        try
+        {
+            projectDetailsString = File.ReadAllText(Directory.GetFiles(args[0], "*.phproj")[0]);
+        }
+        catch (Exception ex)
+        {
+            throw new ArgumentException("Could not read *.phproj file from directory: " + args[0], ex);
+        }
+        
+        Project? project = JsonSerializer.Deserialize<Project>(projectDetailsString);
+        Console.WriteLine(project.Name + " - " + project.Author + ":\n" + project.Scripts[0]);
     }
 }
---
+++ PhosphorDotNet/Project.cs
@@ -0,0 +1,8 @@
+﻿namespace PhosphorDotNet;
+
+public class Project
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public string[] Scripts { get; set; }
+}
